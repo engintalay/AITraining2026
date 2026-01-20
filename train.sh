b7810915f68f0a3d22b5fef23f5c52733faa7a7b
Engin Talay <engintalay@gmail.com>
2026-01-20 21:55:59 +0300

    gtx1650 içinde otomatik config düzelemesi yapildi

--- train.sh
+++ train.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
 deactivate
 source venv/bin/activate
-python main.py --config config.yaml
+# GPU kontrolünü yap ve sonucu bir değişkene ata
+GPU_CHECK=$(lspci | grep -i 'GeForce GTX 1650 Mobile' | wc -l)
+# Eğer sonuç 1 ise GTX 1650 konfigürasyonuyla çalıştır
+if [ "$GPU_CHECK" -eq 1 ]; then
+    echo "GTX 1650 Mobile tespit edildi. Özel konfigürasyon yükleniyor..."
+    python main.py --config config_gtx1650.yaml
+else
+    echo "GTX 1650 bulunamadı. Varsayılan konfigürasyon yükleniyor..."
+    python main.py --config config.yaml
+fi
